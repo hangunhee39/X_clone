fb45fd1f79fb00538a531336232990e9739d6c6d
hangunhee39 <ghee0831@naver.com>
2024-08-20 21:41:27 +0900

    Revert "chore"
    This reverts commit 2c6e87b6060896118ee1f4d0145ff694e7f9fd1d.

--- x-clone/src/app/(afterLogin)/home/page.tsx
+++ x-clone/src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,3 @@
-import Post from '../_component/Post';
-import PostForm from './_component/PostForm';
 import Tab from './_component/Tab'
 import TabProvider from './_component/TabProvider'
 import style from './home.module.css'
@@ -9,13 +7,6 @@ export default function Home() {
     <main className={style.main}>
       <TabProvider>
        <Tab />
-       <PostForm />
-       <Post />
-       <Post />
-       <Post />
-       <Post />
-       <Post />
-       <Post />
       </TabProvider>
     </main>
   );
